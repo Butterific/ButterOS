ac4d799212e76af435a27c5076c86fc4e6057e44
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-13 19:20:56 -0400

    made the apps resizable
    and added the simply epic app!!

--- scripts/openapp.js
+++ scripts/openapp.js
@@ -1,10 +1,14 @@
 var sepic = document.getElementById("sepic");
 var calcapp = document.getElementById("calcapp");
-
+var counter = 1
 
 function opensimplyepic() {
+    counter += 1;
     sepic.style.display = "flex";
+    sepic.style.zIndex = counter;
 }
 function opencalcapp() {
+    counter += 1;
     calcapp.style.display = "flex";
+    calcapp.style.zIndex = counter;
 }
