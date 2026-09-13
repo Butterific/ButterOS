114400e099f22d4c6fc215b93f4ceef3440188dc
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-13 19:02:32 -0400

    add calc app open function

--- scripts/openapp.js
+++ scripts/openapp.js
@@ -1,6 +1,10 @@
 var sepic = document.getElementById("sepic");
+var calcapp = document.getElementById("calcapp");
 
 
 function opensimplyepic() {
     sepic.style.display = "flex";
+}
+function opencalcapp() {
+    calcapp.style.display = "flex";
 }
