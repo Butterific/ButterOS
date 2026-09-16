e8438790f0f3c0937d6d84e9435548123ce83c35
Butterifc <butterificstudios@gmail.com>
2026-09-16 17:40:57 +0000

    finsished the calculator and added the proper app styiling

---
+++ apps/calculator.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+
+function appendtodisplay(input) {
+    if (display.value.includes('Error')) { 
+    display.value = display.value.replace('Error', ''); 
+    }
+    display.style.color = '#1cd880';
+    display.value += input;
+}
+function calculate() { 
+    if (display.value === 'Error') {
+        display.style.color = '#1cd880';
+        display.value = '';
+    }
+    try {   
+    display.style.color = '#1cd880';
+    display.value = eval(display.value);
+    } catch (error) {
+        display.style.color = 'red';
+        display.value = 'Error';
+    }
+}
+function clearDisplay() {
+    display.style.color = '#1cd880';
+    display.value = '';
+}
