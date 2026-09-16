5d50340156d19933d84f5908b16e486de33e5c92
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-15 23:47:01 -0400

    remove mc from openapp.js

--- scripts/openapp.js
+++ scripts/openapp.js
@@ -1,6 +1,5 @@
 var sepic = document.getElementById("sepic");
 var calcapp = document.getElementById("calcapp");
-var minecraftapp = document.getElementById("minecraftapp");
 var counter = 1
 
 function opensimplyepic() {
@@ -12,9 +11,4 @@ function opencalcapp() {
     counter += 1;
     calcapp.style.display = "flex";
     calcapp.style.zIndex = counter;
-}
-function openmcapp() {
-    counter += 1;
-    minecraftapp.style.display = "flex";
-    minecraftapp.style.zIndex = counter;
 }
