1991893324c89d7e2235fc12ce33b7b403509fff
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-15 23:47:28 -0400

    Update openapp.js

--- scripts/openapp.js
+++ scripts/openapp.js
@@ -1,12 +1,13 @@
 var sepic = document.getElementById("sepic");
 var calcapp = document.getElementById("calcapp");
 var counter = 1
-
+// open simply epic
 function opensimplyepic() {
     counter += 1;
     sepic.style.display = "flex";
     sepic.style.zIndex = counter;
 }
+//  open calc
 function opencalcapp() {
     counter += 1;
     calcapp.style.display = "flex";
