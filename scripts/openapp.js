1b0ce29d2b13b72932423d7c606e20170d8b1bb4
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-18 23:38:32 -0400

    Added the components for the music app
    just the files and app formating, no actual app html yet

--- scripts/openapp.js
+++ scripts/openapp.js
@@ -1,5 +1,6 @@
 var sepic = document.getElementById("sepic");
 var calcapp = document.getElementById("calcapp");
+var music = document.getElementById("music");
 var counter = 1
 // open simply epic
 function opensimplyepic() {
@@ -12,4 +13,10 @@ function opencalcapp() {
     counter += 1;
     calcapp.style.display = "flex";
     calcapp.style.zIndex = counter;
+}
+// open music
+function openmusic() {
+    counter += 1;
+    music.style.display = "flex";
+    music.style.zIndex = counter;
 }
