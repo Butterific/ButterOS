d8de0f81582c7dd135452c9ec8e5a23874d28ec0
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-19 16:24:32 -0400

    added the uploading music script/code

--- apps/music/music.js
+++ apps/music/music.js
@@ -1,7 +1,9 @@
 const inpt = document.getElementById("playlistinput");
 const inpt2 = document.getElementById("trackinput");
+const inpt3 = document.getElementById("fileinput");
 const btn = document.getElementById("loadplaylistbutton");
 const btn2 = document.getElementById("loadtrackbutton");
+const btn3 = document.getElementById("musicupload");
 const iframe = document.querySelector("iframe");
 btn.addEventListener("click", () => {
     const playlistname = inpt.value.trim();
@@ -17,5 +19,21 @@ btn2.addEventListener("click", () => {
         iframe.src = `https://streamables.butterlabs.app/embed/${trackname}`;
     } else {
         alert("Enter a valid track name");
+    }
+});
+btn3.addEventListener("click", async () => {
+    const file = inpt3.files[0];
+    if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        const name2 = prompt("Enter a name for your track:");
+        if (!name2) return;
+        formData.append("name", name2);
+        formData.append("protected", "false");
+        await fetch("https://streamables.butterlabs.app/upload", {
+            method: "POST",
+            body: formData
+        });
+        iframe.src=`https://streamables.butterlabs.app/embed/${name2}`;
     }
 });
