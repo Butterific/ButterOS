3ec2679f80f3cbfa3e3125d0a19717883c99b959
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-19 14:06:32 -0400

    e

--- apps/music/music.js
+++ apps/music/music.js
@@ -1,5 +1,7 @@
 const inpt = document.getElementById("playlistinput");
+const inpt2 = document.getElementById("trackinput");
 const btn = document.getElementById("loadplaylistbutton");
+const btn2 = document.getElementById("loadtrackbutton");
 const iframe = document.querySelector("iframe");
 btn.addEventListener("click", () => {
     const playlistname = inpt.value.trim();
@@ -7,5 +9,13 @@ btn.addEventListener("click", () => {
         iframe.src = `https://streamables.butterlabs.app/embed/playlist/${playlistname}`;
     } else {
         alert("Enter a valid playlist name");
+    }
+});
+btn2.addEventListener("click", () => {
+    const trackname = inpt2.value.trim();
+    if (trackname) {
+        iframe.src = `https://streamables.butterlabs.app/embed/${trackname}`;
+    } else {
+        alert("Enter a valid track name");
     }
 });
