7bf2ff568505bd3453dea6b75828debc740600f4
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-21 23:28:15 -0400

    add the files and config to make the cmd app

--- scripts/openapp.js
+++ scripts/openapp.js
@@ -2,6 +2,7 @@ var sepic = document.getElementById("sepic");
 var calcapp = document.getElementById("calcapp");
 var music = document.getElementById("music");
 var info = document.getElementById("info");
+var cmd = document.getElementById("cmd");
 var counter = 1
 // open simply epic
 function opensimplyepic() {
@@ -26,4 +27,10 @@ function openinfo() {
     counter += 1;
     info.style.display = "flex";
     info.style.zIndex = counter;
+}
+// open cmd
+function opencmd() {
+    counter += 1;
+    cmd.style.display = "flex";
+    cmd.style.zIndex = counter;
 }
